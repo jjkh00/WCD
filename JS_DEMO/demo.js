0c2e623b4da79b106c38f1e10960409b154c0d4b
Khoo Jing Jie <jjkhoo.18@ichat.sp.edu.sg>
2018-07-19 19:11:17 +0800

    Testing

---
+++ JS_DEMO/demo.js
@@ -0,0 +1,37 @@
+
+    /*
+      Demo JavaScript
+      Filename: demo.js
+      Author:   khoo jing jie
+      Date:     16/7/18
+      HTML5 and CSS3 
+    */
+
+var hi = "Hello";
+var num = 113;
+num += 10;
+
+console.log(num);
+console.log("Hello, Welcome to javascript");
+console.log("error here"+num);
+console.log("line 3");
+//call a function
+f1();
+
+//access divChange
+var divChange = document.querySelector("#divChange");
+// just for checking
+console.log(divChange);
+divChange.textContent = hi + num;
+
+//create function
+function f1() {
+    console.log("Hello, Welcome to javascript");
+    console.log("error here"+num);
+    console.log("line 3");
+}
+
+// add event listener to my special link
+var clickMe = document.querySelector(".clickMe");
+console.log(clickMe);
+clickMe.addEventListener("click",f1);
---
+++ unitM/script.js
@@ -0,0 +1,34 @@
+/* 
+   Revisions Bookstore and Cafe 
+   Filename: script.js
+
+   Author:   khoo jing jie
+   Date:     16/7/18
+   HTML5 and CSS3 Illustrated Unit M, Visual Workshop
+ */
+var smallersizeBtn = document.querySelector(".smaller-size");
+var defaultsizeBtn = document.querySelector(".selected-button");
+var biggersizeBtn = document.querySelector(".larger-size");
+var html = document.querySelector("html");
+
+function smallersize() {
+  smallersizeBtn.className = "selected-button";
+  html.className = "smaller-size";
+  defaultsizeBtn.className = "html, default-button";
+  biggersizeBtn.className = "larger-size";
+}
+function biggersize(){
+  biggersizeBtn.className = "selected-button";
+  html.className = "larger-size";
+  defaultsizeBtn.className = "html, default-button";
+  smallersizeBtn.className = "smaller-size";
+}
+function defaultsize(){
+  defaultsizeBtn.className = "selected-button";
+  html.className = "html";
+  biggersizeBtn.className = "larger-size";
+  smallersizeBtn.className = "smaller-size";
+}
+var clickSmaller = smallersizeBtn.addEventListener("click",smallersize);
+var clickBigger = biggersizeBtn.addEventListener("click",biggersize);
+var clickDefault = defaultsizeBtn.addEventListener("click", defaultsize);
